fda8bed73d63f0d7d91d07c912fe52ce02a09e51
Gunwoo Lee <Gunwoo@wustl.edu>
2020-12-15 05:05:44 +0900

    dd

---
+++ hotel/client/main.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import Vue from 'vue';
+import App from '../imports/ui/App.vue';
+import VueMeteorTracker from 'vue-meteor-tracker';
+ 
+// Vue.use(VueMeteorTracker);
+// Meteor.startup(() => {
+//   new Vue({
+//     el: '#app',
+//     ...App,
+//   });
+// });
+Vue.use(VueMeteorTracker);
+Meteor.startup(() =>{
+  new 
+}
---
+++ hotel/imports/ui/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div className="container">
+    <header>
+      <h1>Todo List</h1>
+      <form className="new-task" @submit.prevent="handleSubmit">
+        <input
+          type="text"
+          placeholder="Type to add new tasks"
+          v-model="newTask"
+        />
+      </form>
+    </header>
+    <ul>
+      <Task v-for="task in tasks" v-bind:key="task._id" v-bind:task="task" />
+    </ul>
+  </div>
+</template>
+ 
+<script>
+import Vue from "vue";
+import Task from "./Task.vue";
+import { Tasks } from "../api/tasks.js";
+
+export default {
+  components: {
+    Task
+  },
+  data() {
+          return {
+      newTask: ""
+    };
+  },
+  methods: {
+    handleSubmit(event) {
+      Tasks.insert({
+        text: this.newTask,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      this.newTask = "";
+    }
+  },
+  meteor: {
+    tasks() {
+      return Tasks.find({}, { sort: { createdAt: -1 } }).fetch();
+    }
+  }
+};
+</script>
